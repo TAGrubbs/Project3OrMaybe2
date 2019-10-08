a37d5452b5aaa424c62c2d721d026d8495833d67
TAGrubbs <toddagrubbs@gmail.com>
2019-10-08 15:19:03 -0500

    Fixed Search and HTML

--- static/js/app_search.js
+++ static/js/app_search.js
@@ -1,8 +1,8 @@
-var v = require('voca');
+// var v = require('voca');
 function buildArtistdata(artist) {
 
     // @TODO: Complete the following function that builds the metadata panel
-    $("tbody").children().remove();
+    
     // Use `d3.json` to fetch the metadata for a sample
     d3.json(`/search/${artist}`).then(function(response){
       // Use d3 to select the panel with id of `#sample-metadata`
@@ -24,14 +24,10 @@ var button = d3.select("#artistButton");
 button.on("click", function(){
   var inputArtist = d3.select("#autoComplete");
   var inputArtistValue = inputArtist.property("value");
-  console.log(inputArtistValue);
-  var fixedInput = v.capitalize(inputArtistValue, true);
-  console.log(fixedInput);
-  console.log("WhattheFUCK?!");
   var myTable = document.getElementById("artist-table");
   var rowCount = myTable.rows.length;
   for (var x=rowCount-1; x>0; x--) {
    myTable.deleteRow(x);
   };
-  buildArtistdata(fixedInput)
+  buildArtistdata(inputArtistValue)
 })
